1c4c90bfd8db0c1632e19b984b0eb1b21494937d
Ansh Jain <anshj@tekion.com>
2023-09-19 12:36:39 +0530

    RunEvent getBallResult code added

--- src/main/java/com/demoproject/cricketapp/beans/RunEvent.java
+++ src/main/java/com/demoproject/cricketapp/beans/RunEvent.java
@@ -3,6 +3,10 @@ package com.demoproject.cricketapp.beans;
 import lombok.*;
 import lombok.experimental.Accessors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -10,8 +14,31 @@ import lombok.experimental.Accessors;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class RunEvent extends BallEvent {
+    static final List<String> run = Arrays.asList("0", "1", "2", "3", "4", "5", "6");
+    static final List<Double> freq = Arrays.asList(0.25, 0.25, 0.2, 0.05, 0.15, 0.0, 0.1);
+    static final List<Double> prefix = Arrays.asList(0.25, 0.5, 0.7, 0.75, 0.90, 0.90, 1.0);
+
+    private int findCielInPrefix(double val)
+    {
+        int high = prefix.size()-1;
+        int low = 0, index = -1;
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(prefix.get(mid) >= val){
+                index = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return index;
+    }
     public String getBallResult() {
-        return "1";
+        double random = Math.random();
+        System.out.println("Random Value: " + random);
+        return run.get(findCielInPrefix(random));
+//        return "1";
     }
 
 }
